c55053fa8e0db2321422a98b0ec6a4f3d68d1c84
Valdir Félix <vfelixjs@gmail.com>
2025-04-09 21:02:33 -0300

    Arq de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -33,4 +34,21 @@ it('Deve exibir uma messagem de erro ao enserir senha inválida', () => {
     cy.get('.woocommerce-error').should('exist')
 });
 
+it('Deve fazer login com sucesso - Usando massa de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, valdir.teste (não é valdir.teste? Sair)')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixtures', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario , { log: false })
+            cy.get('#password').type(dados.senha , { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, valdir.teste (não é valdir.teste? Sair)')
+
+        })
+        
+        });
 })
